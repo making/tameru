7ec6289ce46a2be39b4cd1d5a4c4d8a4bf6502e7
Toshiaki Maki <makingx@gmail.com>
2024-01-04 23:16:21 +0900

    Add TSV dump

--- src/main/java/am/ik/tameru/query/web/QueryController.java
+++ src/main/java/am/ik/tameru/query/web/QueryController.java
@@ -1,5 +1,6 @@
 package am.ik.tameru.query.web;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,23 +8,31 @@ import am.ik.tameru.event.LogEvent;
 import am.ik.tameru.event.LogEventQuery;
 import am.ik.tameru.event.LogEventQuery.SearchRequest;
 import am.ik.tameru.event.filter.Filter;
+import am.ik.tameru.json.Json;
 import am.ik.yavi.core.ConstraintViolationsException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 @RestController
 public class QueryController {
 
 	private final LogEventQuery logEventQuery;
 
-	public QueryController(LogEventQuery logEventQuery) {
+	private final ObjectMapper objectMapper;
+
+	public QueryController(LogEventQuery logEventQuery, ObjectMapper objectMapper) {
 		this.logEventQuery = logEventQuery;
+		this.objectMapper = objectMapper;
 	}
 
 	@GetMapping(path = "")
@@ -33,6 +42,29 @@ public class QueryController {
 				StringUtils.hasText(filter) ? Filter.parser().parse(filter) : null);
 		SearchRequest.validator.validate(searchRequest).throwIfInvalid(ConstraintViolationsException::new);
 		return this.logEventQuery.findLatestLogEvents(searchRequest);
+	}
+
+	@GetMapping(path = "", produces = "text/tsv")
+	public ResponseEntity<StreamingResponseBody> eventsAsTsv(@RequestParam(required = false) String query,
+			@RequestParam(defaultValue = "30") int size, @RequestParam(required = false) String filter) {
+		List<LogEvent> events = this.events(query, size, filter);
+		StreamingResponseBody stream = outputStream -> {
+			int i = 1;
+			outputStream.write("eventId	timestamp	message	metadata".getBytes(StandardCharsets.UTF_8));
+			outputStream.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
+			for (LogEvent event : events) {
+				outputStream.write("%d	%s	%s	%s"
+						.formatted(event.eventId(), event.timestamp(), event.message(),
+								Json.stringify(this.objectMapper, event.metadata()))
+						.getBytes(StandardCharsets.UTF_8));
+				outputStream.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
+				if (i++ % 30 == 0) {
+					outputStream.flush();
+				}
+			}
+			outputStream.flush();
+		};
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "text/tsv").body(stream);
 	}
 
 	@GetMapping(path = "/{eventId}")
